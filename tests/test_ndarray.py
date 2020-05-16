0d0a42d52f333373fbc9d23d387a42e145f0335b
CrepeGoat <awqatty.b@gmail.com>
2020-05-15 20:50:18 -0400

    added tests for `Indexer.sliced`

--- tests/test_ndarray.py
+++ tests/test_ndarray.py
@@ -44,3 +44,78 @@ def test_indexer_make_basic(shape, expt_shape, expt_offset, expt_strides):
     assert indexer._shape == expt_shape
     assert indexer._offset == expt_offset
     assert indexer._strides == expt_strides
+
+
+@pytest.mark.parametrize('indexer, index, expt_indexer', [
+    (
+        ndarray.Indexer.make_basic(shape=(3,)), 0,
+        ndarray.Indexer(shape=(), offset=0, strides=()),
+    ),
+    (
+        ndarray.Indexer.make_basic(shape=(3,)), 1,
+        ndarray.Indexer(shape=(), offset=1, strides=()),
+    ),
+    (
+        ndarray.Indexer.make_basic(shape=(3,)), slice(1, None),
+        ndarray.Indexer(shape=(2,), offset=1, strides=(1,)),
+    ),
+    (
+        ndarray.Indexer.make_basic(shape=(3,)), slice(None, None, -1),
+        ndarray.Indexer(shape=(3,), offset=2, strides=(-1,)),
+    ),
+
+    (
+        ndarray.Indexer.make_basic(shape=(3,)), (Ellipsis, 1),
+        ndarray.Indexer(shape=(), offset=1, strides=()),
+    ),
+    (
+        ndarray.Indexer.make_basic(shape=(3,)), (Ellipsis, slice(1, None)),
+        ndarray.Indexer(shape=(2,), offset=1, strides=(1,)),
+    ),
+
+    (
+        ndarray.Indexer.make_basic(shape=(5, 3, 7)), 0,
+        ndarray.Indexer(shape=(3, 7), offset=0, strides=(7, 1)),
+    ),
+    (
+        ndarray.Indexer.make_basic(shape=(5, 3, 7)), 3,
+        ndarray.Indexer(shape=(3, 7), offset=63, strides=(7, 1)),
+    ),
+
+    (
+        ndarray.Indexer.make_basic(shape=(5, 3, 7)), slice(None),
+        ndarray.Indexer(shape=(5, 3, 7), offset=0, strides=(21, 7, 1)),
+    ),
+    (
+        ndarray.Indexer.make_basic(shape=(5, 3, 7)), slice(1, None),
+        ndarray.Indexer(shape=(4, 3, 7), offset=21, strides=(21, 7, 1)),
+    ),
+    (
+        ndarray.Indexer.make_basic(shape=(5, 3, 7)), slice(None, None, -1),
+        ndarray.Indexer(shape=(5, 3, 7), offset=84, strides=(-21, 7, 1)),
+    ),
+
+    (
+        ndarray.Indexer.make_basic(shape=(5, 3, 7)),
+        (slice(None), 1),
+        ndarray.Indexer(shape=(5, 7), offset=7, strides=(21, 1)),
+    ),
+    (
+        ndarray.Indexer.make_basic(shape=(5, 3, 7)),
+        (slice(None), slice(1, None)),
+        ndarray.Indexer(shape=(5, 2, 7), offset=7, strides=(21, 7, 1)),
+    ),
+
+    (
+        ndarray.Indexer.make_basic(shape=(5, 3, 7)),
+        (Ellipsis, 1),
+        ndarray.Indexer(shape=(5, 3), offset=1, strides=(21, 7)),
+    ),
+    (
+        ndarray.Indexer.make_basic(shape=(5, 3, 7)),
+        (Ellipsis, slice(1, None)),
+        ndarray.Indexer(shape=(5, 3, 6), offset=1, strides=(21, 7, 1)),
+    ),
+])
+def test_indexer_sliced(indexer, index, expt_indexer):
+    assert indexer.sliced(index) == expt_indexer
